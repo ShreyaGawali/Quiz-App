5c2c75c976d0a24f53effe35365f59215402d897
Shreya Gawali <gawalishreya7@gmail.com>
2024-01-03 22:35:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Quiz from './Component/quiz';
+
+
+function App() {
+
+  return (
+    <>
+      <Quiz/>
+    </>
+  );
+}
+
+export default App;
